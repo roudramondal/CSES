dfbf6c77bbc7b3b3eab86aeb9201ce5bbd2a61bf
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-11-13 16:44:28 +0600

    Add files via upload

---
+++ Weird_Algo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+  long long  n;
+  scanf("%lld", &n);
+  
+  while (n>1)
+  {
+  printf("%lld ", n);
+  if (n%2==0)
+  {
+    n = n / 2;
+  }
+  else 
+  {
+    n = 3*n + 1;
+  }
+  
+  }
+  printf("%lld\n", n);
+  return 0;
+}
